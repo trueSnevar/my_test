003e5fe8b7d7de8401b051b60b575ca103745c8f
Alex <aknyazev87@gmail.com>
2020-02-06 23:01:03 +0300

    Make changes to models and register them to admin site

--- trucks/admin.py
+++ trucks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import TruckModel, Truck
 
 # Register your models here.
+
+admin.site.register(TruckModel)
+admin.site.register(Truck)
--- trucks/models.py
+++ trucks/models.py
@@ -7,8 +7,14 @@ class TruckModel(models.Model):
     model_name = models.CharField(max_length=200)
     weight_capacity = models.FloatField()
 
+    def __str__(self):
+        return self.model_name
+
 
 class Truck(models.Model):
-    garage_number = models.FloatField(unique=True)
+    garage_number = models.CharField(max_length=200)
     model = models.ForeignKey(TruckModel, on_delete=models.CASCADE)
+    current_weight = models.FloatField(default=0)
 
+    def __str__(self):
+        return self.garage_number
