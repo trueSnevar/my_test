b8235d8e1d9bff3ae5b0b0057518e0c11a57977a
Alex <aknyazev87@gmail.com>
2020-02-08 22:28:43 +0300

    Adjusted view to filter form by a model, finished the first version of the task

--- trucks/views.py
+++ trucks/views.py
@@ -5,7 +5,20 @@ from django.template import loader
 # Create your views here.
 
 
+def is_valid_queryparam(param):
+    return param != '' and param is not None
+
+
 def index(request):
     truck_list = Truck.objects.all()
-    context = {'truck_list': truck_list}
+    models = TruckModel.objects.all()
+    model = request.GET.get('model')
+
+    if is_valid_queryparam(model) and model != "Все":
+        truck_list = truck_list.filter(model=model)
+
+    context = {
+        'truck_list': truck_list,
+        'models': models
+    }
     return render(request, 'trucks/index.html', context)
