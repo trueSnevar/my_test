4e8d19524eff37cd9168af40381f4093ad4ebf01
aknyazev <aknyazev87@gmail.com>
2020-02-10 17:27:09 +0300

    Added zero division validation to TruckModel field

--- trucks/models.py
+++ trucks/models.py
@@ -1,11 +1,20 @@
 from django.db import models
-
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
+
+
+def validate_zero(value):
+    if value == 0:
+        raise ValidationError(
+            _('%(value)s является недопустимым значением'),
+            params={'value': value},
+        )
 
 
 class TruckModel(models.Model):
     model_name = models.CharField(max_length=200)
-    weight_capacity = models.FloatField()
+    weight_capacity = models.FloatField(blank=False, default=100, validators=[validate_zero])
 
     def __str__(self):
         return self.model_name
