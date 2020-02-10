cc376d826c72fffc4897d49e95eb171b5ef9c01b
aknyazev <aknyazev87@gmail.com>
2020-02-10 17:48:17 +0300

    Apdated allowed hosts

--- mytest/settings.py
+++ mytest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r0$bg@x$erh#jyk8zqqr5l#1agj=npwbqbz!a(0yqufc!g*scl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://snevar.pythonanywhere.com/']
 
 
 # Application definition
